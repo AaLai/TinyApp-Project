c4b1a1f72d73802ec88fb456ebb2028cfb094a4c
aaron <aaron_lai@hotmail.com>
2018-10-31 20:35:20 +0000

    Added url_reg, modified existanceChecker to accept more inputs, added paths for registration pages but pages still not connected

--- express_server.js
+++ express_server.js
@@ -9,7 +9,20 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
-var urlDatabase = {
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -39,14 +52,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  if (!existanceChecker(urlDatabase, req.params.shortURL)) {
-    res.send(`404 error, this doesn't exist yet! I do like your ${req.params.shortURL} though!`);
-  } else {
-    let longURL = urlDatabase[req.params.shortURL];
-  // console.log(longURL);  // debug statement!
-    res.redirect(longURL);
-  }
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -70,23 +77,68 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+
+app.get("/u/:shortURL", (req, res) => {
+  if (!existanceChecker(urlDatabase, req.params.shortURL)) {
+    res.send(`404 error, this doesn't exist yet! I do like your ${req.params.shortURL} though!`);
+  } else {
+    let longURL = urlDatabase[req.params.shortURL];
+  // console.log(longURL);  // debug statement!
+    res.redirect(longURL);
+  }
 });
+
 
 app.post("/login", (req, res) => {
   res.cookie('Kow', req.body.username)
   res.redirect('/urls');
 })
 
+
 app.post("/logout", (req, res) => {
   res.clearCookie('Kow');
   res.redirect('/urls');
 })
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+app.get("/register", (req, res) => {
+  let templateVars = { url: urlDatabase,
+                       username: req.cookies['Kow']
+                     };
+  res.render("urls_reg", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  let randomID = generateRandomString();
+  if (!req.body.email || !req.body.password) {
+    res.send("400 Bad Request: Make sure both fields are filled in!");
+  } else if () {
+  } else if (!users[randomID]) {
+      users[randomID] = { id: randomID,
+                          email: req.body.email,
+                          password: req.body.password
+                        };
+                        // console.log(users)
+    res.cookie('Kow', req.body.email);
+    res.redirect("/urls");
+  } else {
+    let randomID2 = generateRandomString();
+       users[randomID2] = { id: randomID2,
+                           email: req.body.email,
+                           password: req.body.password
+                         };
+                         // console.log(users)
+    res.cookie('Kow', req.body.email);
+    res.redirect("/urls");
+
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -98,6 +150,12 @@ app.listen(PORT, () => {
       return r;
     }
 
-  const existanceChecker = (database, input) => {
-    return database[input];
+  const existanceChecker = (database, input, input2, input3) => {
+    if (input3 !== undefined) {
+      return database[input][input2][input3];
+    } else if (input2 !== undefined) {
+      return database[input][input2];
+    } else {
+      return database[input];
+    }
   }
