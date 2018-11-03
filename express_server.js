946c32003026c29443f4847479844c74ba8bc7c1
aaron <aaron_lai@hotmail.com>
2018-11-03 01:30:02 +0000

    Debugging, code refactoring, stylistic changes to site, added URL error messages based on assignment sheet specifications

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
+
+// --- Requiring and setting apps
 const express = require("express");
 const cookieParser = require('cookie-parser');
-const app = express();
 const bodyParser = require("body-parser");
-const PORT = 8080; // default port 8080
 const cookieSession = require('cookie-session');
-
 const bcrypt = require('bcrypt');
+const PORT = 8080;
+const app = express();
 
 
 app.set("view engine", "ejs");
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
+
+// --- User Database
 
 const users = {
   "userRandomID": {
@@ -27,205 +30,222 @@ const users = {
   }
 }
 
-const urlDatabase = { "b2xVn2": { 'user_id' : 'userRandomID',
-                                  'longurl' : 'http://www.lighthouselabs.ca'
-                                },
-                      "9sm5xK": { 'user_id' : 'userRandomID',
-                                  'longurl' : 'http://www.google.com'
-                                },
-                      "testme": { 'user_id' : 'user2RandomID',
-                                  'longurl' : 'https://mangadex.org'
-                                }
-                    };
+// --- Database for long and short URls
+
+const urlDatabase = {
+  "b2xVn2": {
+    user_id: 'userRandomID',
+    longurl: 'http://www.lighthouselabs.ca'
+  },
+  "9sm5xK": {
+    user_id: 'userRandomID',
+    longurl: 'http://www.google.com'
+  },
+  "testme": { user_id : 'user2RandomID',
+    longurl: 'https://mangadex.org'
+  }
+}
+
+// --- URLs for song management UI
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.session.user_id && users[req.session.user_id]) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase,
-                       user: users,
-                       login: req.session.user_id
-                     };
-  res.render("urls_index", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  var randString = generateRandomString();
-  // console.log(req.body);
-  // console.log(req.body.longURL);
-  urlDatabase[randString] = { longurl : req.body.longURL };
-  urlDatabase[randString].user_id = req.session.user_id ;
-  // console.log(urlDatabase)   //debug statement!
-  res.redirect(`/urls/${randString}`);
+  let templateVars = {
+    urls: urlDatabase,
+    user: users,
+    login: req.session.user_id
+  }
+  if (req.session.user_id) {
+    if (!users[req.session.user_id]) {
+      res.send('403 Error');
+    } else {
+      res.render("urls_index", templateVars);
+    }
+  } else {
+    res.send('403 Error, please log in.')
+  }
 });
 
 app.get("/urls/new", (req, res) => {
-  if (!req.session.user_id) {
-    res.redirect('/login');
+  if (users[req.session.user_id]) {
+    let templateVars = {
+      user: users,
+      login: req.session.user_id,
+      urls: urlDatabase
+    }
+    res.render("urls_new", templateVars);
   } else {
-  let templateVars = { user: users ,
-                       login: req.session.user_id,
-                       urls: urlDatabase };
-  res.render("urls_new", templateVars);
+    res.redirect('/login');
   }
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id,
-                       urls: urlDatabase,
-                       user: users,
-                       login: req.session.user_id
-                     };
-  if (req.session.user_id === urlDatabase[req.params.id].user_id) {
+  let templateVars = {
+    shortURL: req.params.id,
+    urls: urlDatabase,
+    user: users,
+    login: req.session.user_id
+  }
+  if (users[req.session.user_id] && req.session.user_id === urlDatabase[req.params.id].user_id) {
     res.render("urls_show", templateVars);
-  } else {
-    res.redirect('/urls')
+  } else if (users[req.session.user_id] && req.session.user_id !== urlDatabase[req.params.id].user_id) {
+    res.send('403 Forbidden');
+  } else if (!urlDatabase[req.params.id]) {
+    res.send('404 Not Found');
+  } else if (!users[req.session.user_id]) {
+    res.send('403 Forbidden');
   }
 });
 
-app.post("/urls/:id/update", (req, res) => {
-  // console.log(req.body.longURLUpdate);
-  if (req.session.user_id === urlDatabase[req.params.id].user_id) {
-    urlDatabase[req.params.id].longurl = req.body.longURLUpdate;
-    res.redirect('/urls');
-  } else {
-    res.redirect('/urls');
-  }
-})
-
-app.post("/urls/:id/delete", (req, res) => {
-  if (req.session.user_id === urlDatabase[req.params.id].user_id) {
-    delete urlDatabase[req.params.id];
-    res.redirect('/urls');
-  } else {
-    res.redirect('/urls');
-  }
-  // console.log(urlDatabase);
-});
-
-
+// --- url to link short URLs to long
 
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
-    res.send(`404 error, this doesn't exist yet! I do like what you did with ${req.params.shortURL} though!`);
+    res.send(`(╯°□°）╯︵ |${req.params.shortURL}| 404 error, this doesn't exist yet!`);
   } else {
     let longURL = urlDatabase[req.params.shortURL].longurl;
-  // console.log(longURL);  // debug statement!
     res.redirect(longURL);
   }
 });
 
+// --- short URL creation and management
+
+app.post("/urls", (req, res) => {
+  if (users[req.session.user_id]) {
+    var randString = generateRandomString();
+    urlDatabase[randString] = { longurl : req.body.longURL };
+    urlDatabase[randString].user_id = req.session.user_id ;
+    res.redirect(`/urls/${randString}`);
+  } else {
+    res.send('403 Forbidden');
+  }
+});
+
+app.post("/urls/:id", (req, res) => {
+  if (users[req.session.user_id] && req.session.user_id === urlDatabase[req.params.id].user_id) {
+    urlDatabase[req.params.id].longurl = req.body.longURLUpdate;
+    res.redirect('/urls');
+  } else if (users[req.session.user_id] && req.session.user_id !== urlDatabase[req.params.id].user_id) {
+    res.send('403 Forbidden');
+  } else if (!users[req.session.user_id]) {
+    res.send('403 Forbidden');
+  }
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  if (users[req.session.user_id] && req.session.user_id === urlDatabase[req.params.id].user_id) {
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+  } else if (users[req.session.user_id] && req.session.user_id !== urlDatabase[req.params.id].user_id) {
+    res.send('403 Forbidden');
+  } else if (!users[req.session.user_id]) {
+    res.send('403 Forbidden');
+  }
+});
+
+// --- User account management URLs
 
 app.get("/login", (req, res) => {
-  let templateVars = { user: users,
-                       urls: urlDatabase,
-                       login: req.session.user_id
+  let templateVars = {
+    user: users,
+    urls: urlDatabase,
+    login: req.session.user_id
   }
-  if (!req.session.user_id) {
-    res.render("urls_login", templateVars);
-  } else if (req.session.user_id === users[req.session.user_id] ) {
+  if (users[req.session.user_id]) {
     res.redirect('/urls');
   } else {
-    res.render("urls_login", templateVars)
+    res.render("urls_login", templateVars);
   }
-})
+});
 
 app.post("/login", (req, res) => {
   let userPassword = bcrypt.compareSync(req.body.password.toString(), filter(users, req.body.email, 'email', 'password').toString())
   if (!req.body.email || !req.body.password) {
-    res.send('400 Error: already you sure everything is filled in?');
+    res.send(' (╯°□°）╯︵ 400 Error: are you sure everything is filled in?');
   } else if (!userPassword) {
-    res.send('400 something didn\'t work out, try again!');
+    res.send(' (╯°□°）╯︵ 400 something didn\'t work out, try again!');
   } else if (!req.session.user_id) {
     req.session.user_id = filter(users, req.body.email, 'email', 'id');
     res.redirect('/urls');
   } else {
     res.redirect('/urls');
   }
-})
-
-
-app.post("/logout", (req, res) => {
-  req.session.user_id = null;
-  res.redirect('/urls');
-})
-
-app.get("/debug", (req, res) => {
-  console.log(users);
-  console.log(urlDatabase);
-  res.redirect('/urls');
-})
-
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect('/');
+});
+
 app.get("/register", (req, res) => {
-  let templateVars = { urls: urlDatabase,
-                       user: users,
-                       login: req.session.user_id
-                     };
-  res.render("urls_reg", templateVars);
+  if (users[req.session.user_id]) {
+    res.redirect('/urls')
+  } else {
+    let templateVars = {
+      urls: urlDatabase,
+      user: users,
+      login: req.session.user_id
+    }
+    res.render("urls_reg", templateVars);
+  }
 });
 
 app.post("/register", (req, res) => {
   let randomID = generateRandomString();
   if (!req.body.email || !req.body.password) {
-    res.send("400 Bad Request: Make sure both fields are filled in!");
+    res.send("(╯°□°）╯︵ |400|  Bad Request: Make sure both fields are filled in!");
   } else if (inObjectChecker(users, req.body.email, 'email')) {
-    res.send("400 Bad Request");
+    res.send("(╯°□°）╯︵ |400| Bad Request");
   } else if (!users[randomID]) {
-      users[randomID] = { id: randomID,
-                          email: req.body.email,
-                          password: bcrypt.hashSync(req.body.password, 10)
-                        };
-                        // console.log(users)
+    users[randomID] = {
+      id: randomID,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10)
+    }
     req.session.user_id = randomID;
     res.redirect("/urls");
   } else {
     let randomID2 = generateRandomString();
-      users[randomID2] = { id: randomID2,
-                           email: req.body.email,
-                           password: bcrypt.hashSync(req.body.password, 10)
-                         };
-                         // console.log(users)
+    users[randomID2] = {
+      id: randomID2,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10)
+    }
     req.session.user_id = randomID2;
     res.redirect("/urls");
   }
 });
 
+// --- Debug console logger
+// app.get("/debug", (req, res) => {
+//   console.log(users);
+//   console.log(urlDatabase);
+//   res.redirect('/');
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// --- Filters and random string generator
 
   function generateRandomString() {
     let r = Math.random().toString(36).substring(7);
       return r;
     }
 
-  const existanceChecker = (database, input, input2, input3) => {
-    if (input3 !== undefined) {
-      return database[input][input2][input3];
-    } else if (input2 !== undefined) {
-      return database[input][input2];
-    } else {
-      return database[input];
-    }
-  }
-
   const inObjectChecker = (object, checkAgainst, input) => {
     let isItThere = false;
     for (let x in object) {
-      if (object[x][input] == checkAgainst) {
+      if (object[x][input] === checkAgainst) {
         isItThere = true;
       }
     }
@@ -235,13 +255,13 @@ app.listen(PORT, () => {
   const filter = (object, checkAgainst, input, output) => {
     const checkAgainstStr = checkAgainst.toString();
     const outputStr = output.toString();
-    let thing = false;
+    let result = false;
     if (input === null) {
       for (let x in object) {
         const keys = Object.keys(object[x]);
         keys.forEach(function(key) {
           if (key === checkAgainstStr) {
-            thing = object[x][key];
+            result = object[x][key];
           }
         });
       }
@@ -249,19 +269,9 @@ app.listen(PORT, () => {
       const inputStr = input.toString();
       for (let x in object) {
         if (object[x][inputStr] === checkAgainstStr) {
-          thing = object[x][outputStr];
+          result = object[x][outputStr];
         }
       }
     }
-    return thing;
-  }
-
-  const specificFilter = (object, checkAgainst1, checkAgainst2, input1, input2) => {
-    let filter = false;
-    for (let x in object) {
-      if (object[x][input1] == checkAgainst1 && object[x][input2] == checkAgainst2) {
-        filter = true;
-      }
-    }
-    return filter;
+    return result;
   }
